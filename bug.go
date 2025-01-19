8865c1312df6092dd771630022a84f9535996fbd
BugHunter-X <askzenai@gmail.com>
2025-01-19 21:40:05 +0100

    Add bug.go file

---
+++ bug.go
@@ -0,0 +1,9 @@
+```go
+func main() {
+    var i int
+    fmt.Println(i) // Output: 0
+    fmt.Println(i == 0) // Output: true
+    fmt.Println(i == 0.0) // Output: true
+    fmt.Println(i == float64(0))
+}
+```
